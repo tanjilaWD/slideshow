b2f3e10ea34254de9795ca561c4baf3462f9d50e
Tanjila Khatun <tanjilabithi12@gmail.com>
2024-06-25 11:05:42 +0600

    new

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
 const slideshowElements = document.querySelectorAll('.slideshow-element');
+ 
+let currentElements = 1;
 
 setInterval(() => {
-    
+    customElements ++;
+  let currentElement = document.querySelector('.current'); 
+  currentElement.classList.remove('current');
+
+  if (currentElements > slideshowElements.length){
+    slideshowElements[0].classList.add('current');
+  } else{
+    currentElement.nextElementSibling.classList.add('current');
+
+  }
+  currentElement.nextElementSibling.classList.add('current');
+
 },2000);
